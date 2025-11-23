50252cc7a71f4a4038eae4a83283dffed45d508c
Julio Torres <julio.torres7@outlook.com>
2025-11-23 18:49:58 -0300

    adicionando teste de compromissos

--- motor_test.py
+++ motor_test.py
@@ -13,3 +13,11 @@ def test_reprovado_restritivo(engine):
 def test_aprovado_restritivo(engine):
     cliente = {"score": 700, "renda": 1500, "situacao": "ok", "compromissos":0, "parcela_prevista":0, "profissao":"analista", "tempo_emprego_meses":12}
     assert engine.avaliar(cliente)["decisao"] == "Aprovado"
+
+def test_manual_score_limiar(engine):
+    cliente = {"score": 455, "renda": 2500, "situacao": "ok", "compromissos":0, "parcela_prevista":0, "profissao":"analista", "tempo_emprego_meses":12}
+    assert engine.avaliar(cliente)["decisao"] in ("Manual","Reprovado","Aprovado")
+
+def test_renda_comprometida(engine):
+    cliente = {"score": 100, "renda": 1500, "situacao": "ok", "compromissos":500, "parcela_prevista":100, "profissao":"analista", "tempo_emprego_meses":12}
+    assert engine.avaliar(cliente)["decisao"] == "Reprovado"
